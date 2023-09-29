355d138a4e90d24e8f4d6c653629fda997cededd
Andre Dragacevac <a.dragacevac@icloud.com>
2023-09-29 00:00:11 -0400

    filter over array and return products matching the inputs state, then map over searchResults to display

--- peoples-place-app/src/components/StockProductList.js
+++ peoples-place-app/src/components/StockProductList.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import ProductCard from './ProductCard'
 
+import { useState } from 'react'
+
 function StockProductList({products}) {
    //accepts products as props
    //destructures products from props obj
+   const [ searchQuery, setSearchQuery ] = useState("");
+   //change what is rendered based on searchQuery
+
+   //searchResults filters through the array of products and returns
+   //product if name matches searchQuery
+   const searchResults = products.filter((product) => {
+      return product.name.toLowerCase().includes(searchQuery.toLowerCase());
+   })
    //maps over products array to render a 
    //ProductCard component for each element 
-      //provide key prop set to product id value
-      //provide each product object as a prop 
-      //to pass to ProductCard
-   const productCard = products.map((product) => {
+   //map over the search results to show filtered products
+   const productCards = searchResults.map((product) => {
       return <ProductCard key={product.id} product={product} />
-   })
+   })//provide key prop set to product id value
+   //provide each product object as a prop 
+   //to pass to ProductCard
+
+   //
+   const handleOnChange = (e) => setSearchQuery(e.target.value)
   return (
-   <ul className='cards'>{productCard}</ul>
+   <section>
+      <h2>All Products</h2>
+      <input type='text' placeholder='Search...' onChange={handleOnChange}></input>
+      <ul className='cards'>{productCards}</ul>
+   </section>
   )
 }
 
