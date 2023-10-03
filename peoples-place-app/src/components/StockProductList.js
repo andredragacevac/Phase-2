8519f5f21911bfe548f03f3bee27b1e26567c6df
Andre Dragacevac <a.dragacevac@icloud.com>
2023-10-03 00:05:12 -0400

    takes onfav callback and onload as props, button onclick to load products from db, pass onfav callback to prod card

--- peoples-place-app/src/components/StockProductList.js
+++ peoples-place-app/src/components/StockProductList.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import { useState } from 'react'
 import ProductCard from './ProductCard'
 
-import { useState } from 'react'
-
-function StockProductList({products}) {
+function StockProductList({ products, onFavoriteProduct, onLoadProducts}) {
    //accepts products as props
    //destructures products from props obj
    const [ searchQuery, setSearchQuery ] = useState("");
@@ -14,19 +12,29 @@ function StockProductList({products}) {
    const searchResults = products.filter((product) => {
       return product.name.toLowerCase().includes(searchQuery.toLowerCase());
    })
+   
    //maps over products array to render a 
    //ProductCard component for each element 
    //map over the search results to show filtered products
    const productCards = searchResults.map((product) => {
-      return <ProductCard key={product.id} product={product} />
+      return (
+         <ProductCard 
+            key={product.id} 
+            product={product}
+            onFavoriteProduct={onFavoriteProduct}
+         />
+      )
    })//provide key prop set to product id value
    //provide each product object as a prop 
    //to pass to ProductCard
 
    //
-   const handleOnChange = (e) => setSearchQuery(e.target.value)
+   const handleOnChange = (e) => {
+      setSearchQuery(e.target.value)
+   }
   return (
    <section>
+      <button onClick={onLoadProducts}>Load Products</button>
       <h2>All Products</h2>
       <input type='text' placeholder='Search...' onChange={handleOnChange}></input>
       <ul className='cards'>{productCards}</ul>
