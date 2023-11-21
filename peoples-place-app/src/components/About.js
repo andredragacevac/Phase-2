c94ef4e37ff10d88eb1e0e7b215158628a35009d
Andre Dragacevac <a.dragacevac@icloud.com>
2023-11-20 23:04:49 -0500

    About page

---
+++ peoples-place-app/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>Welcome to the Peoples Place! We are a sneaker/streetwear consignment store with a vast amount a variety of products from streetwear, to sneakers, and accesories. </p>
+    </section>
+  )
+}
+
+export default About
