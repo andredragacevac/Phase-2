535cb4bb9f4c05abb8a211d6a5f1b5c9b3ea72fa
Andre Dragacevac <a.dragacevac@icloud.com>
2023-11-20 23:07:30 -0500

    Set up links for routes in Header component

--- peoples-place-app/src/components/Header.js
+++ peoples-place-app/src/components/Header.js
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header>
-      
+      <h1>
+        <Link className="button" to="/">The Peoples Place</Link>
+      </h1>
       <nav>
         <div className="navigation">
           <Link className="button" to="/requests">Requests</Link>
-          <Link className="button" to="/requests/new">Request Products</Link>
+          <Link className="button" to="/requests/new">Request Product</Link>
+          <Link className="button" to="/about">About Us</Link>
         </div>
       </nav>
     </header>
