ea4d36202fe051f30feb63304ff5031cfe240364
Andre Dragacevac <a.dragacevac@icloud.com>
2023-11-20 23:18:59 -0500

    Moved code from RequestContainer up to App.js. Imported components for routing. Created routes and passed props.

--- peoples-place-app/src/App.js
+++ peoples-place-app/src/App.js
@@ -1,16 +1,35 @@
 import { Routes, Route } from "react-router";
+import { useState, useEffect } from "react";
 
+import About from "./components/About";
 import Header from "./components/Header";
-import RequestProductContainer from "./components/RequestProductContainer";
 import StockProductList from "./components/StockProductList";
+import RequestProductForm from "./components/RequestProductForm";
+import RequestList from "./components/RequestList";
 
 function App() {
+  const [ requests, setRequests ] = useState([]);
+
+  const onSubmitRequest = (product) => {
+    setRequests([ ...requests, product])
+  }
+
+  useEffect(() => {
+    let url = "http://localhost:4000/requests";
+    fetch(url)
+      .then((res) => res.json())
+      .then((requestData) => setRequests(requestData));
+  }, [])
+
   return (
     <div className="App">
+      
       <Header />
       <Routes>
         <Route exact path="/" element= {<StockProductList />} />
-        <Route path="/requests" element= {<RequestProductContainer />} />
+        <Route path="/requests" element= {<RequestList requests={requests}/>} />
+        <Route path="/requests/new" element= {<RequestProductForm onSubmitRequest={onSubmitRequest}/>} />
+        <Route path="/about" element= {<About />} />
       </Routes>
     </div>
   );
